52dfe6d26c64397d079f7a7b614895da51559a69
anushkashr <99557370+anushkashr@users.noreply.github.com>
2023-08-01 17:53:06 -0500

    Authorized login

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspCoreIdentity.WebApp.Controllers
+{
+    [Authorize]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
