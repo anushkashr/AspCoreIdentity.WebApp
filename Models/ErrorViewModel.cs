f2dd72b5a37e8128cb6b8f6c6315f104cb54604a
anushkashr <99557370+anushkashr@users.noreply.github.com>
2023-08-01 13:39:05 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspCoreIdentity.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
