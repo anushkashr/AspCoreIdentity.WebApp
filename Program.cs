5946b7a139905b3171e91e9978c53c0460a7a238
anushkashr <99557370+anushkashr@users.noreply.github.com>
2023-08-01 15:05:10 -0500

    Specified connection string and added DbContext

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
+
+using Identity.DataAccessLayer.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
