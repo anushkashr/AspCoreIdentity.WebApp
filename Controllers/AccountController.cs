f4cc95ab5bc14d90f5ab6ab8fa1f2e682caacbc9
anushkashr <99557370+anushkashr@users.noreply.github.com>
2023-08-01 22:22:57 -0500

    Created login page with validations

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using AspCoreIdentity.WebApp.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspCoreIdentity.WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel model)
+		{
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("index", "Home");
+                }
+                ModelState.AddModelError("", "Invalid email or password provided");
+            }
+			return View(model);
+		}
+		public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
